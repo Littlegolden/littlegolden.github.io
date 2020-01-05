9445807a3417c36a98d240b7beefb89f7d15a062
Golden <littlegoldenjin@gmail.com>
2020-01-05 21:01:34 +0800

    Add CDN Paymen

--- donate/assets/js.js
+++ donate/assets/js.js
@@ -1,35 +1,38 @@
 window.onload = function() {
-
+// 支付宝-赞助
 	document.getElementById("alipay").onclick = function() {
 		document.getElementById("qrcodeAli").className = "qrcode show showIn"
 	};
-
 	document.getElementById("closeBtn1").onclick = function() {
 		document.getElementById("qrcodeAli").className = "qr"
 	};
-
+// 微信-赞助
 	document.getElementById("wechat").onclick = function() {
 		document.getElementById("qrcodeWechat").className = "qrcode show showIn"
 	};
-
 	document.getElementById("closeBtn2").onclick = function() {
 		document.getElementById("qrcodeWechat").className = "qr"
 	};
-
+// QQ-赞助
 	document.getElementById("qq").onclick = function() {
 		document.getElementById("qrcodeQQ").className = "qrcode show showIn"
 	};
-
 	document.getElementById("closeBtn3").onclick = function() {
 		document.getElementById("qrcodeQQ").className = "qr"
 	};
-	
+// Venmo-赞助
 	document.getElementById("venmo").onclick = function() {
 		document.getElementById("qrcodeVenmo").className = "qrcode show showIn"
 	};
-	
 	document.getElementById("closeBtn4").onclick = function() {
 		document.getElementById("qrcodeVenmo").className = "qr"
+	}
+// 四合一-服务器
+	document.getElementById("MgpayS1").onclick = function() {
+		document.getElementById("qrcodeMgpayS1").className = "qrcode show showIn"
+	};
+	document.getElementById("closeBtnMgpayS1").onclick = function() {
+		document.getElementById("qrcodeMgpayS1").className = "qr"
 	}
 	
 }
